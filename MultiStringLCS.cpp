af3a93733254063a3f457dd7cbd00a54cf3a1ceb
Collin Lowing <collin.lowing@protonmail.com>
2022-11-22 11:56:05 -0600

    printSimilarityTable

--- MultiStringLCS.cpp
+++ MultiStringLCS.cpp
@@ -3,3 +3,33 @@
 //
 
 #include "MultiStringLCS.hpp"
+
+char MultiStringLCS::getSimilarity(char *stringX, char *stringY) {
+    return 0;
+}
+
+void MultiStringLCS::printSimilarityTable() {
+    for(int i = 0; i < numberOfStrings; i++) {
+        if(i <= 9) {
+            std::cout << "0" << i << " ";
+        }
+        else { // greater than 9 so do not add 0 to beginning
+            std::cout << i << " ";
+        }
+    }
+
+    std::cout << std::endl;
+
+    for(int m = 0; m < numberOfStrings; m++) {
+        if(m <= 9) {
+            std::cout << "0" << m << " ";
+        }
+        else { // greater than 9 so do not add 0 to beginning
+            std::cout << m << " ";
+        }
+        for(int n = 0; n < numberOfStrings; n++) {
+            std::cout << similarityTable[m][n] << " ";
+        }
+        std::cout << std::endl;
+    }
+}
--- MultiStringLCS.hpp
+++ MultiStringLCS.hpp
@@ -5,9 +5,17 @@
 #ifndef COP4534_P5_MULTISTRINGLCS_HPP
 #define COP4534_P5_MULTISTRINGLCS_HPP
 
+#include <iostream>
 
 class MultiStringLCS {
-
+private:
+    char** similarityTable;
+    char** stringTable;
+    int* stringLengths;
+    int numberOfStrings;
+public:
+    char getSimilarity(char* stringX, char* stringY);
+    void printSimilarityTable();
 };
 
 
